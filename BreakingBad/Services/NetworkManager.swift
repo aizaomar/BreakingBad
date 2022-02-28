38fbb25c96f0b8d2f2bcc46151412fc5832ac70d
Aizaomar <amanbekova1@gmail.com>
2022-02-28 22:14:59 +0600

    Edits network manager

--- BreakingBad/Services/NetworkManager.swift
+++ BreakingBad/Services/NetworkManager.swift
@@ -7,42 +7,29 @@
 
 import Foundation
 
-
-//   Дождусь разбора домашки :)
-
-
-
-
-
-
-
-
-
-
-//class NetworkManager {
-//
-//    static let shared = NetworkManager()
-//
-//    private init() {}
-//
-//    func fetchCharacters() {
-//        guard let url = URL(string: Link.breakingBadApi.rawValue) else { return }
-//
-//        URLSession.shared.dataTask(with: url) { data, _, error in
-//            guard let data = data else {
-//                print(error?.localizedDescription ?? "No error description")
-//                return
-//            }
-//            do {
-//                self.characters = try JSONDecoder().decode([Character].self, from: data)
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-//            } catch let error {
-//                print(error.localizedDescription)
-//            }
-//
-//        }.resume()
-//
-//    }
-//}
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchCharacter(from url: String, completion: @escaping(Character) -> Void) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "no descripption")
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(Character.self, from: data)
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+}
